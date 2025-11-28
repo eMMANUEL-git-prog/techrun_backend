300bfff61946327e04792513fd534c77d899d7c3
HP-Probook <emmanuel19727@gmail.com>
2025-11-28 15:46:11 +0300

    stk push

--- src/routes/payments.routes.js
+++ src/routes/payments.routes.js
@@ -1,12 +1,13 @@
-const express = require("express")
-const router = express.Router()
-const paymentsController = require("../controllers/payments.controller")
-const { authenticateToken } = require("../middleware/auth")
+const express = require("express");
+const router = express.Router();
+const paymentsController = require("../controllers/payments.controller");
+const { authenticateToken } = require("../middleware/auth");
 
-router.use(authenticateToken)
+router.post("/mpesa/callback", paymentsController.mpesaCallback);
 
-router.post("/mpesa/stk-push", paymentsController.initiateMpesaPayment)
-router.post("/mpesa/callback", paymentsController.mpesaCallback)
-router.get("/transactions", paymentsController.getTransactions)
+// Protected routes
+router.use(authenticateToken);
+router.post("/mpesa/stk-push", paymentsController.initiateMpesaPayment);
+router.get("/transactions", paymentsController.getTransactions);
 
-module.exports = router
+module.exports = router;
